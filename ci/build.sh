036228e98444a784d016e97465eeeae3b3845795
Logan Hanks <logan.hanks@gmail.com>
2016-01-16 14:37:31 -0800

    puts

--- ci/build.sh
+++ ci/build.sh
@@ -7,5 +7,6 @@ NODE_ENV=production node_modules/.bin/webpack
 
 if [ ${DRONE_BRANCH} == master -o ${DRONE_BRANCH} == drone ]; then
   cd build
-  s3cmd -M sync . s3://alpha.euphoria.io
+  s3cmd put -m text/css *.css s3://alpha.euphoria.io
+  s3cmd put *.html *.js *.map s3://alpha.euphoria.io
 fi
