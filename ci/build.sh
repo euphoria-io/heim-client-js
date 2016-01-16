5b40caf13e101ded301eb59249e09ae35a0c985a
Logan Hanks <logan.hanks@gmail.com>
2016-01-16 14:32:07 -0800

    Pass -M to s3cmd sync

--- ci/build.sh
+++ ci/build.sh
@@ -7,5 +7,5 @@ NODE_ENV=production node_modules/.bin/webpack
 
 if [ ${DRONE_BRANCH} == master -o ${DRONE_BRANCH} == drone ]; then
   cd build
-  s3cmd sync . s3://alpha.euphoria.io
+  s3cmd -M sync . s3://alpha.euphoria.io
 fi
