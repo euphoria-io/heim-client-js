574994fc05a40d1c6146d6388260e79031d3b547
Logan Hanks <logan.hanks@gmail.com>
2016-01-16 16:30:58 -0800

    Add namespace prefix to actions

--- const/index.js
+++ const/index.js
@@ -1,7 +1,9 @@
-export const SWITCH_ROOM = 'SWITCH_ROOM'
+const Action = a => '@@heim-client/'+a
 
-export const WS_CONNECTING = 'WS_CONNECTING'
-export const WS_CONNECTED = 'WS_CONNECTED'
-export const WS_DISCONNECTED = 'WS_DISCONNECTED'
-export const WS_MESSAGE_RECEIVED = 'WS_MESSAGE_RECEIVED'
-export const WS_MESSAGE_SENT = 'WS_MESSAGE_SENT'
+export const SWITCH_ROOM = Action('SWITCH_ROOM')
+
+export const WS_CONNECTING = Action('WS_CONNECTING')
+export const WS_CONNECTED = Action('WS_CONNECTED')
+export const WS_DISCONNECTED = Action('WS_DISCONNECTED')
+export const WS_MESSAGE_RECEIVED = Action('WS_MESSAGE_RECEIVED')
+export const WS_MESSAGE_SENT = Action('WS_MESSAGE_SENT')
