1d006b54ac32e4b0b513ea668dfd93da5da8b170
Logan Hanks <logan.hanks@gmail.com>
2016-01-18 09:54:23 -0800

    Add caching for node_modules in drone builds

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,25 @@
 
 set -ex
 
-npm install
+NODE_MODULES_REV=$(git log --pretty=format:%H package.json | head -n 1)
+
+if s3cmd get s3://alpha.euphoria.io/node_modules-${NODE_MODULES_REV}.tar.gz node_modules.tar.gz; then
+  echo 'Using cached node_modules'
+  tar xzf node_modules.tar.gz
+else
+  echo 'Installing node_modules'
+  npm install
+  echo 'Caching node_modules'
+  tar cf node_modules-${NODE_MODULES_REV}.tar node_modules
+  gzip -9 node_modules-${NODE_MODULES_REV}.tar
+  s3cmd put node_modules-${NODE_MODULES_REV}.tar.gz s3://alpha.euphoria.io
+fi
+
+echo 'Building static assets'
 NODE_ENV=production node_modules/.bin/webpack
 
 if [ ${DRONE_BRANCH} == master -o ${DRONE_BRANCH} == drone ]; then
+  echo 'Uploading static assets'
   cd build
   s3cmd put -m text/css *.css s3://alpha.euphoria.io
   s3cmd put *.html *.js *.map s3://alpha.euphoria.io
