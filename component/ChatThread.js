f3aed68fa77c724b92ba4bae7e89b5745ce5f89a
Logan Hanks <logan.hanks@gmail.com>
2016-01-20 12:14:12 -0800

    Move message rendering into own component

--- component/ChatThread.js
+++ component/ChatThread.js
@@ -1,65 +1,24 @@
-import moment from 'moment'
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 
-import { nickBgColor, nickBgLightColor } from '../lib/nick'
+import { nickBgColor } from '../lib/nick'
 import Tree from '../lib/Tree'
 
-import Timestamp from './Timestamp'
-import UserText from './UserText'
+import Message from './Message'
 
 class ChatThread extends Component {
   render() {
-    const { tree, parentId } = this.props
+    const { parentId, roomName, tree } = this.props
     const msg = parentId ? tree.get(parentId) : null
+    const msgNode = msg ? <Message msgId={msg.id} roomName={roomName} /> : null
     const children = tree.childrenOf(parentId)
     const childrenNode = !!children.size ? this.renderChildren(children, msg && msg.sender.name) : null
-    const msgNode = this.renderMessage(msg, !!children.size)
 
     return (
       <div className="thread">
         {msgNode}
         {childrenNode}
-      </div>
-    )
-  }
-
-  renderMessage(msg, withChildren) {
-    if (!msg) {
-      return ''
-    }
-
-    let messageStyle = {}
-    let nickStyle = nickBgColor(msg.sender.name)
-    if (withChildren) {
-      nickStyle = {
-        ...nickStyle,
-        borderBottomLeftRadius: 0,
-      }
-      messageStyle = {
-        ...messageStyle,
-        borderTopLeftRadius: '0.3rem',
-        borderLeftStyle: 'solid',
-        borderLeftWidth: '2px',
-        borderLeftColor: nickStyle.background,
-      }
-    }
-
-
-    const emote = msg.content.startsWith('/me ')
-    const className = emote ? 'message emote' : 'message'
-    const content = emote ? msg.content.substr(4) : msg.content
-    const contentStyle = emote ? nickBgLightColor(msg.sender.name) : null
-    return (
-      <div className={className} style={messageStyle}>
-        <div className="sender">
-          <UserText className="nick" style={nickStyle}>{msg.sender.name}</UserText>
-        </div>
-        <div className="content-and-time">
-          <UserText className="content" style={contentStyle}>{content}</UserText>
-          <Timestamp at={moment.unix(msg.time)} />
-        </div>
       </div>
     )
   }
---
+++ component/Message.js
@@ -0,0 +1,61 @@
+import moment from 'moment'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import { nickBgColor, nickBgLightColor } from '../lib/nick'
+
+import Timestamp from './Timestamp'
+import UserText from './UserText'
+
+class Message extends Component {
+  render() {
+    const { hasChildren, msg } = this.props
+    let messageStyle = {}
+    let nickStyle = nickBgColor(msg.sender.name)
+    if (hasChildren) {
+      nickStyle = {
+        ...nickStyle,
+        borderBottomLeftRadius: 0,
+      }
+      messageStyle = {
+        ...messageStyle,
+        borderTopLeftRadius: '0.3rem',
+        borderLeftStyle: 'solid',
+        borderLeftWidth: '2px',
+        borderLeftColor: nickStyle.background,
+      }
+    }
+
+    const emote = msg.content.startsWith('/me ')
+    const className = emote ? 'message emote' : 'message'
+    const content = emote ? msg.content.substr(4) : msg.content
+    const contentStyle = emote ? nickBgLightColor(msg.sender.name) : null
+    return (
+      <div className={className} style={messageStyle}>
+        <div className="sender">
+          <UserText className="nick" style={nickStyle}>{msg.sender.name}</UserText>
+        </div>
+        <div className="content-and-time">
+          <UserText className="content" style={contentStyle}>{content}</UserText>
+          <Timestamp at={moment.unix(msg.time)} />
+        </div>
+      </div>
+    )
+  }
+}
+
+Message.propTypes = {
+  msgId: PropTypes.string.isRequired,
+  roomName: PropTypes.string.isRequired,
+}
+
+function select(state, props) {
+  const { msgId, roomName } = props
+  const chat = state.chatSwitch.chats.get(roomName)
+  return {
+    hasChildren: chat.tree.hasChildren(msgId),
+    msg: chat.tree.get(msgId),
+  }
+}
+
+export default connect(select)(Message)
