27e2a0eba62ee204d6569dadf72970a002116c19
Logan Hanks <logan.hanks@gmail.com>
2016-01-25 13:19:22 -0800

    Add ChatEntry resizing on multine input

--- component/ChatEntry.js
+++ component/ChatEntry.js
@@ -4,14 +4,26 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 import Nick from './Nick'
 
 class ChatEntry extends Component {
+  resize() {
+    const { input, measure } = this.refs
+    measure.style.width = input.offsetWidth + 'px'
+    measure.value = input.value
+    input.style.height = measure.scrollHeight + 'px'
+  }
+
   render() {
     const { id, nick } = this.props
+    const onChange = () => {
+      this.resize()
+    }
+    const onClick = onChange
     return (
       <div id={id} className="chat-entry">
         <div className="sender">
           <Nick name={nick || 'your name here'} />
         </div>
-        <textarea defaultValue="chat here" />
+        <textarea ref="input" defaultValue="chat here" onChange={onChange} onClick={onClick} />
+        <textarea ref="measure" className="measure" />
       </div>
     )
   }
